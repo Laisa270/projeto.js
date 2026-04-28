c8fc9ab985534827cfa410bc71f149dd8dab6c26
Laisa Yasmin <laisayasmiin270@gmail.com>
2026-04-28 20:34:05 -0300

    Atualizando arquivos da pasta

---
+++ AULA05/exemplo4.js
@@ -0,0 +1,16 @@
+import { leitorDeNumeros } from "./leitorDeNumeros";
+let valor = 12;
+let lista = ["2,3,4,5"]
+console.log(lista [0]);
+console.log(lista [2]);
+console.log(lista [0] + lista[1]);
+console.log(valor)
+console.log(lista)
+function leExibe(quantidade){
+let numero= leitorDeNumeros("Digite um número:");
+for (let index=0; index<quantidade;index++){
+    let numero= leitorDeNumeros("Digite um número:");
+
+
+}
+}
